5ddccf6338e79ab90ec64331cb492023a121a841
DRomanov <dromanovtech@gmail.com>
2026-08-28 06:30:13 +0300

    go to week1

---
+++ week1/ba05/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn parse_bitmap_8x8(lines: [&str; 8]) -> [u8; 8] {
+    todo!()
+}
+pub fn render_bitmap_8x8(bytes: [u8; 8]) -> [String; 8] {
+    todo!()
+}
+pub fn invert_bitmap_8x8(bytes: [u8; 8]) -> [u8; 8] {
+    todo!()
+}
